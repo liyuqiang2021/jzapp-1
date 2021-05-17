e03114b52157be20870ded894c0a36f0e20776d1
john <liyuqiang2021@outlook.com>
2021-05-17 23:48:09 +0800

    修复value被赋值的问题

--- src/components/Money/FromItem.vue
+++ src/components/Money/FromItem.vue
@@ -2,7 +2,12 @@
   <div>
     <label class="fromItem">
       <span class="name">{{ this.fieldName }}</span>
-      <input type="text" v-model="value" :placeholder="this.placeholder" />
+      <input
+        type="text"
+        :value="value"
+        @input="onValueChanged($event.target.value)"
+        :placeholder="this.placeholder"
+      />
     </label>
   </div>
 </template>
@@ -13,13 +18,13 @@ import { Component, Prop, Watch } from "vue-property-decorator";
 
 @Component
 export default class FromItem extends Vue {
-  value = "";
+  @Prop({ default: "" }) readonly value!: string;
 
   @Prop({ required: true }) fieldName!: string;
   @Prop() placeholder?: string;
 
   @Watch("value")
-  onValueChanged(value: string, oldValue: string) {
+  onValueChanged(value: string) {
     this.$emit("update:value", value);
   }
 }

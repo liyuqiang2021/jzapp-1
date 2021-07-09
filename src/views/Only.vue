5811a604485c678e1f3ba80de36e06002e57642d
john <liyuqiang2021@outlook.com>
2021-07-09 19:25:14 +0800

    部署网站到jzapp-1-website

--- src/views/Only.vue
+++ src/views/Only.vue
@@ -1,8 +1,21 @@
 <template>
-  <Layout> 展示 </Layout>
+  <Layout>
+    <div class="view">
+      <p>请 使 用 手 机 预 览</p>
+    </div>
+  </Layout>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 export default class Statistics extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+.view {
+  position: relative;
+  top: 120px;
+  text-align: center;
+  font-size: 18px;
+}
+</style>

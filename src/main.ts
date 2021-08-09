bb03a254aba967fdf45e3af7498ab4eea72b9d2d
john <liyuqiang2021@outlook.com>
2021-08-09 16:02:35 +0800

    完善欢迎页

--- src/main.ts
+++ src/main.ts
@@ -41,7 +41,7 @@ window.onload = function () {
 };
 
 if (document.documentElement.clientWidth > 500) {
-  window.alert('请使用手机打开本页面，以保证浏览效果');
+  window.alert('建议使用手机打开本页面，以保证浏览效果');
   const img = document.createElement('img');
   img.src = '/qrcode.png';
   img.style.position = 'fixed';
--- src/views/Only.vue
+++ src/views/Only.vue
@@ -1,7 +1,11 @@
 <template>
   <Layout>
     <div class="view">
-      <p>请 使 用 手 机 预 览</p>
+      <!-- <p class="halo">欢 迎 使 用 极 简 记</p> -->
+      <img src="/jz.png" class="halo" />
+      <div class="mon">
+        <Icon name="money" class="money" />
+      </div>
     </div>
   </Layout>
 </template>
@@ -12,10 +16,29 @@ export default class Statistics extends Vue {}
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
 .view {
   position: relative;
   top: 120px;
   text-align: center;
   font-size: 18px;
+}
+.halo {
+  // color: $color-highlight;
+  position: relative;
+  top: -30px;
+  width: 300px;
+  // box-shadow: 0 0 6px 0 rgba($color: #fc655a, $alpha: 1);
+}
+.mon {
+  width: 100vw;
+  height: 50px;
+  .money {
+    position: relative;
+    top: 50px;
+    min-width: 50px;
+    min-height: 50px;
+    fill: red;
+  }
 }
 </style>
